cd582c84ef67e2da67c16617dfe628e6153ba78d
Nikhil Baravkar <baravkar.nikhil05@gmail.com>
2023-11-01 12:23:02 +0530

    cicd pipeline added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -103,7 +103,7 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/NikhilBaravkarDataScientist/my-first-repo.mlflow",
+            mlflow_uri="https://dagshub.com/NikhilBaravkarDataScientist/EndtoEndMLwithDockerAWS.mlflow",
            
         )
 
